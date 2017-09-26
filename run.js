ab739bf9f47642a20e51e7c0513603a1a6d20606
Bg aQin <backme80@debian-8.personal-pc>
2017-09-26 14:31:58 +0700

    test nodejs

--- run.js
+++ run.js
@@ -1,2 +1,3 @@
-// testgithub
-console.log('run')
+let fna = (next) => { return next(1) },
+    fnb = (a, next) => { return fna((val) => { return next((2 * a) + val) }) }
+fnb(4, (val) => { console.log(val) })
